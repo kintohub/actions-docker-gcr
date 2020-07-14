65a070239a63a7522e5f65fe4ece8a4a8d58bc8b
bappr <ben@kintohub.com>
2020-07-14 15:29:55 +0800

    🐛 Add retry logic to argocd

--- argocd/entrypoint.sh
+++ argocd/entrypoint.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-argocd app actions run \
-  --server $INPUT_SERVER \
-  --kind $INPUT_KIND \
-  --namespace $INPUT_NAMESPACE \
-  --resource-name $INPUT_NAME \
-  $INPUT_APP $INPUT_ACTION
+n=0
+until [ "$n" -ge 5 ]
+do
+  argocd app actions run \
+    --server $INPUT_SERVER \
+    --kind $INPUT_KIND \
+    --namespace $INPUT_NAMESPACE \
+    --resource-name $INPUT_NAME \
+    $INPUT_APP $INPUT_ACTION && break
+  n=$((n+1))
+  sleep 5
+done
