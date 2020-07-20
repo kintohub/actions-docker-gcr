843545be2fba7d4fbf6b661421221c76625bd796
Nandi Wong <nandi0315@gmail.com>
2020-07-20 12:12:11 +0800

    fix script

--- sentry/entrypoint.sh
+++ sentry/entrypoint.sh
@@ -18,13 +18,11 @@ fi
 echo "Pulling $INPUT_REGISTRY/$INPUT_IMAGE:$INPUT_TAG"
 docker pull $INPUT_REGISTRY/$INPUT_IMAGE:$INPUT_TAG
 
-docker run images $INPUT_REGISTRY/$INPUT_IMAGE:$INPUT_TAG
-
 export SENTRY_ORG=$INPUT_SENTRY_ORG
 export SENTRY_PROJECT=$INPUT_SENTRY_PROJECT
 export SENTRY_AUTH_TOKEN=$INPUT_SENTRY_TOKEN
 
-docker run --entrypoint '/bin/sh' asia.gcr.io/kinto-tools/kinto-frontend:v1.0.6 -c '
+docker run --entrypoint '/bin/sh' $INPUT_REGISTRY/$INPUT_IMAGE:$INPUT_TAG -c '
 yarn global add @sentry/cli
 sentry-cli releases list
 sentry-cli releases new "$INPUT_TAG" 
