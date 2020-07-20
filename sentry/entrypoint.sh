aa40ab8003d7efc3416481267bf70a4c1a598f14
Nandi Wong <nandi0315@gmail.com>
2020-07-20 14:35:45 +0800

    ❄️feat: add sentry action (#2)
    * add sentry action
    
    * test version
    
    * fix script
    
    * debug
    
    * debug
    
    * fix template
    
    * fix script
    
    * fix path
    
    * use github sha:

---
+++ sentry/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+: ${INPUT_REGISTRY:=gcr.io}
+: ${INPUT_IMAGE:=$GITHUB_REPOSITORY}
+: ${INPUT_TAG:=$GITHUB_SHA}
+
+if [ -n "${INPUT_GCLOUD_KEY}" ]; then
+  echo "Logging into gcr.io with INPUT_GCLOUD_KEY..."
+  echo ${INPUT_GCLOUD_KEY} | base64 --decode --ignore-garbage > /tmp/key.json
+  gcloud auth activate-service-account --quiet --key-file /tmp/key.json
+  gcloud auth configure-docker --quiet
+else
+  echo "INPUT_GCLOUD_KEY was empty, not performing auth" 1>&2
+fi
+
+echo "Pulling $INPUT_REGISTRY/$INPUT_IMAGE:$INPUT_TAG"
+docker pull $INPUT_REGISTRY/$INPUT_IMAGE:$INPUT_TAG
+
+echo "Running sentry script"
+
+# use the GITHUB_SHA as the version as we need to have that on FE as well
+docker run --entrypoint '/bin/sh' $INPUT_REGISTRY/$INPUT_IMAGE:$INPUT_TAG -c '
+yarn global add @sentry/cli
+export SENTRY_ORG='$INPUT_SENTRY_ORG'
+export SENTRY_PROJECT='$INPUT_SENTRY_PROJECT'
+export SENTRY_AUTH_TOKEN='$INPUT_SENTRY_TOKEN'
+sentry-cli releases list
+sentry-cli releases new '$GITHUB_SHA'
+sentry-cli releases finalize '$GITHUB_SHA'
+sentry-cli releases files '$GITHUB_SHA' upload-sourcemaps static/js/ --rewrite --url-prefix "~/static/js"
+'
